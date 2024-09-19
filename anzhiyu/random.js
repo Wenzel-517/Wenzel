091c3fab9142ba5da2f782e0e1dff6f51d34915e
Wenzel-517 <zws517@qq.com>
2024-09-19 22:52:24 +0800

    Site updated: 2024-09-19 22:52:24

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/06/21/标签语法/","2022/11/25/心路历程/","2024/09/19/测试/","2024/07/10/艾宾浩斯遗忘曲线/"];function toRandomPost(){
+var posts=["2022/11/25/心路历程/","2024/06/21/标签语法/","2024/09/19/测试/","2024/07/10/艾宾浩斯遗忘曲线/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
